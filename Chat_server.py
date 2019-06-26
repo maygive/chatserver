13042fdf485d083c8f4bfe88f1eec44f76b772c2
maygive <46147791+maygive@users.noreply.github.com>
2019-06-26 17:25:47 +0700

    Update Chat_server.py

--- Chat_server.py
+++ Chat_server.py
@@ -19,7 +19,7 @@ try:
     
     while True:
         
-        db = pymysql.connect("127.0.0.1","root","heart650","CHAT_SERVER")
+        db = pymysql.connect("127.0.0.1","root","123456","CHAT_SERVER")
         cursor = db.cursor()
         
         data, addr = s.recvfrom(1024)
